cbb3661b5789ad06c2d93331a8a447c21b9bbd9e
Dima <dmi.aksyonov@gmail.com>
2020-11-02 11:23:09 +0300

    Added VC for ToDo addition

--- ToDo/ViewControllers/AdditionViewController.swift
+++ ToDo/ViewControllers/AdditionViewController.swift
@@ -11,19 +11,21 @@ class AdditionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
     
+    @IBOutlet weak var tableView: UITableView!
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension AdditionViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
