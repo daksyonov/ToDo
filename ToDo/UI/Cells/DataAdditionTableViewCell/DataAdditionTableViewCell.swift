e379f40f44849da2f403f8aec9d72624d69f3bc7
Dima <dmi.aksyonov@gmail.com>
2020-11-02 12:48:20 +0300

    Added Cell UI

--- ToDo/UI/Cells/DataAdditionTableViewCell/DataAdditionTableViewCell.swift
+++ ToDo/UI/Cells/DataAdditionTableViewCell/DataAdditionTableViewCell.swift
@@ -9,15 +9,18 @@ import UIKit
 
 class DataAdditionTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        self.selectionStyle = .none
+        
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func setup(bgColor: UIColor, title: String) {
+        self.backgroundColor = bgColor
+        self.titleLabel.text = title
     }
-
+    
 }
