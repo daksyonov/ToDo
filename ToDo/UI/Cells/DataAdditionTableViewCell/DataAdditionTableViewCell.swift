62962b6a57d43c6126b3baef72a1e2d2ffc5c31c
Dima <dmi.aksyonov@gmail.com>
2020-11-03 16:37:00 +0300

    Cell + UIDatePicker

--- ToDo/UI/Cells/DataAdditionTableViewCell/DataAdditionTableViewCell.swift
+++ ToDo/UI/Cells/DataAdditionTableViewCell/DataAdditionTableViewCell.swift
@@ -10,10 +10,13 @@ import UIKit
 class DataAdditionTableViewCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentSubView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    
+    private var datePicker = UIDatePicker()
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         self.selectionStyle = .none
         
     }
@@ -23,4 +26,55 @@ class DataAdditionTableViewCell: UITableViewCell {
         self.titleLabel.text = title
     }
     
+    func configureDatePicker() {
+        self.datePicker.datePickerMode = .dateAndTime
+        self.datePicker.minimumDate = Date()
+        self.datePicker.preferredDatePickerStyle = .compact
+        self.datePicker.locale = Locale.current
+        self.datePicker.backgroundColor = .clear
+        self.datePicker.addTarget(
+            self,
+            action: #selector(self.datePickerValueChanged),
+            for: .valueChanged
+        )
+        
+        let toolbar = UIToolbar()
+        
+        let todayButton = UIBarButtonItem(
+            title: "Today",
+            style: .plain,
+            target: self,
+            action: #selector(self.todayButtonPressed)
+        )
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(self.doneButtonPressed)
+        )
+        let flexibleSpace = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: self,
+            action: nil
+        )
+        
+        let toolBar = UIToolbar()
+        toolbar.setItems(
+            [todayButton, flexibleSpace, doneButton],
+            animated: true
+        )
+        toolbar.barStyle = .black
+        toolbar.isTranslucent = true
+        toolbar.sizeToFit()
+        
+        self.textField.inputView = self.datePicker
+//        self.textField.inputAccessoryView = toolbar
+        
+    }
+    
+    @objc func datePickerValueChanged(_ datePicker: UIDatePicker) {
+        self.textField.text = self.datePicker.date.formattedString
+    }
+    @objc func todayButtonPressed() {}
+    @objc func doneButtonPressed() {}
 }
+
